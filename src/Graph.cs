29cc5fe797af6a3eb6f600e695e4f22b64852e11
EthanC2 <ethanrcox@gmail.com>
2022-03-15 20:43:32 -0400

    added abstract functions to base class

--- src/Graph.cs
+++ src/Graph.cs
@@ -5,6 +5,11 @@ public abstract class Graph<T, TVertex> where TVertex : Vertex<T>
 {
     protected Dictionary<T, TVertex> Vertices {get; set;}
 
+    public abstract void AddVertex(T name);
+    public abstract void RemoveVertex(T name);
+    public abstract void AddEdge(T from, T to);
+    public abstract void RemoveEdge(T from, T to);
+    
     public Graph()
     {
         Vertices = new Dictionary<T, TVertex>();
