4c8b4485573212705d103a0d5a49290e0bd4d916
EthanC2 <ethanrcox@gmail.com>
2022-03-17 21:16:46 -0400

    ...

--- src/Vertex.cs
+++ src/Vertex.cs
@@ -5,12 +5,33 @@ using System.Collections.Generic;
 public class Vertex<T>
 {
     public T Name {get; init;} 
-    public HashSet<Vertex<T>> Neighbors {get; protected set;}
+    protected HashSet<Vertex<T>> Neighbors {get; set;}
 
     public Vertex(T name)
     {
         Name = name;
         Neighbors = new HashSet<Vertex<T>>();        
+    }
+    
+    public virtual void AddNeighbor(params Vertex<T>[] vertices)
+    {
+        foreach (var vertex in vertices)
+        {
+            Neighbors.Add(vertex);
+        }
+    }
+
+    public virtual void RemoveNeighbor(params Vertex<T>[] vertices)
+    {
+        foreach (var vertex in vertices)
+        {
+            Neighbors.Remove(vertex);
+        }
+    }
+
+    public virtual bool AreConnected(Vertex<T> from, Vertex<T> to)
+    {
+        return from.Neighbors.Contains(to) || to.Neighbors.Contains(from);
     }
 
     public override string ToString()
